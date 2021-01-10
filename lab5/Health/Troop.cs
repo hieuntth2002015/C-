d9123937f6a146d13be36471800d928cd52e0768
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-01-10 23:25:46 +0700

    ss8

---
+++ lab5/Health/Troop.cs
@@ -0,0 +1,14 @@
+using System
+
+namespace Health
+{
+	class Troops
+    {
+        private int _id;
+        private string _name;
+        private string location;
+        private int numgroup;
+
+        public void Create()
+    }
+}
