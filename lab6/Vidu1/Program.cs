c31caae8548c7df70589b9a33c58fb74a1c60866
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-01-15 08:20:00 +0700

    lab7

---
+++ lab6/Vidu1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidu1
+{
+    abstract class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Every animal eats food in order to survice");
+        }
+        public abstract void AnimalSound();
+    }
+    class Lion: Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Lion roars");
+        }
+        static void Main(string[] args)
+        {
+            Lion objLion = new Lion();
+            objLion.AnimalSound();
+            objLion.Eat();
+        }
+    }
+}
